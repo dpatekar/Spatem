cb093761c75c0652326465ad8bbbe571a4467b60
drazen <dpatekar@gmail.com>
2019-05-16 09:44:01 +0200

    Add files

---
+++ Spatem.Api/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Spatem.Core.Identity;
+using Spatem.Data.Ef;
+using NJsonSchema;
+using NSwag.AspNetCore;
+
+namespace Spatem.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDataContext(Configuration.GetConnectionString("SpatemConnection"));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddIdentityDataContext()
+                .AddDefaultTokenProviders();
+
+            services.AddSwaggerDocument(settings =>
+            {
+                settings.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Spatem API";
+                    document.Info.Description = "REST API";
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUi3();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ Spatem.Data.Ef/StartMigration.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host 'Listing existing migrations...'
+
+$StartupProject = '../Spatem.Api'
+dotnet ef --startup-project $StartupProject migrations list
+
+$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+$decision = $Host.UI.PromptForChoice('Generating new migration...','Are you sure you want to proceed?', $choices, 1)
+if ($decision -eq 0) {
+  Write-Host 'Confirmed'
+  $MigrationName = Read-Host -Prompt 'Enter a name for new migration'
+  dotnet ef --startup-project $StartupProject migrations add $MigrationName
+} else {
+  Write-Host 'Cancelled'
+}
+
+$decision = $Host.UI.PromptForChoice('Applying latest migration...','Are you sure you want to proceed?', $choices, 1)
+if ($decision -eq 0) {
+  Write-Host 'Confirmed'
+  dotnet ef --startup-project $StartupProject database update
+} else {
+  Write-Host 'Cancelled'
+}
